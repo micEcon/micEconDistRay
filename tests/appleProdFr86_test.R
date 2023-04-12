0b8442fb2343d39e3167ee66891e8e708a7c5a80
arne-henningsen <arne.henningsen@gmail.com>
2023-04-12 11:57:14 +0200

    added tests for distRayEla() and distRayDeriv()

--- tests/appleProdFr86_test.R
+++ tests/appleProdFr86_test.R
@@ -6,7 +6,7 @@ appleProdFr86$qCap <- appleProdFr86$vCap / appleProdFr86$pCap
 appleProdFr86$qLab <- appleProdFr86$vLab / appleProdFr86$pLab
 appleProdFr86$qMat <- appleProdFr86$vMat / appleProdFr86$pMat
 
-# Cobb-Douglas ray-based input distance function
+### Cobb-Douglas ray-based input distance function
 estCD <- distRayEst( xNames = c( "qCap", "qLab", "qMat" ),
   yNames = c( "qApples", "qOtherOut" ),
   data = appleProdFr86, form = "cd" )
@@ -16,7 +16,22 @@ lapply( estCD$coefList, function(x) round( x, 3 ) )
 apply( estCD$mono, 2, table )
 lapply( estCD$ela, function(x) round( summary(x), 2 ) )
 
-# Translog ray-based input distance function
+# calculate elasticities
+elaCD <- distRayEla( xNames = c( "qCap", "qLab", "qMat" ),
+  yNames = c( "qApples", "qOtherOut" ),
+  coef = coef( estCD ), data = appleProdFr86, form = "cd" )
+all.equal( elaCD, estCD$ela )
+lapply( elaCD, function(x) round( summary(x), 2 ) )
+
+# calculate derivatives
+derivCD <- distRayDeriv( xNames = c( "qCap", "qLab", "qMat" ),
+  yNames = c( "qApples", "qOtherOut" ),
+  coef = coef( estCD ), data = appleProdFr86, form = "cd" )
+lapply( derivCD, function(x) round( summary(x), 
+  3 - round( log( max( abs( x ) ), 10 ) ) ) )
+
+
+### Translog ray-based input distance function
 estTL <- distRayEst( xNames = c( "qCap", "qLab", "qMat" ),
   yNames = c( "qApples", "qOtherOut" ),
   data = appleProdFr86 )
@@ -25,4 +40,18 @@ cbind( names( estTL ) )
 lapply( estTL$coefList, function(x) round( x, 3 ) )
 apply( estTL$mono, 2, table )
 lapply( estTL$ela, function(x) round( summary(x), 2 ) )
+
+# calculate elasticities
+elaTL <- distRayEla( xNames = c( "qCap", "qLab", "qMat" ),
+  yNames = c( "qApples", "qOtherOut" ),
+  coef = coef( estTL ), data = appleProdFr86 )
+all.equal( elaTL, estTL$ela )
+lapply( elaTL, function(x) round( summary(x), 2 ) )
+
+# calculate derivatives
+derivTL <- distRayDeriv( xNames = c( "qCap", "qLab", "qMat" ),
+  yNames = c( "qApples", "qOtherOut" ),
+  coef = coef( estTL ), data = appleProdFr86 )
+lapply( derivTL, function(x) round( summary(x), 
+  3 - round( log( max( abs( x ) ), 10 ) ) ) )
 
--- tests/germanFarms_test.R
+++ tests/germanFarms_test.R
@@ -4,7 +4,7 @@ library( "micEconDistRay" )
 data( germanFarms, package = "micEcon" )
 germanFarms$qVarInput <- germanFarms$vVarInput / germanFarms$pVarInput
 
-# Cobb-Douglas ray-based input distance function
+### Cobb-Douglas ray-based input distance function
 estCD <- distRayEst( xNames = c( "land", "qLabor", "qVarInput" ),
   yNames = c( "vCrop", "vAnimal" ),
   data = germanFarms, form = "cd" )
@@ -14,7 +14,22 @@ lapply( estCD$coefList, function(x) round( x, 3 ) )
 apply( estCD$mono, 2, table )
 lapply( estCD$ela, function(x) round( summary(x), 2 ) )
 
-# Translog ray-based input distance function
+# calculate elasticities
+elaCD <- distRayEla( xNames = c( "land", "qLabor", "qVarInput" ),
+  yNames = c( "vCrop", "vAnimal" ),
+  coef = coef( estCD ), data = germanFarms, form = "cd" )
+all.equal( elaCD, estCD$ela )
+lapply( elaCD, function(x) round( summary(x), 2 ) )
+
+# calculate derivatives
+derivCD <- distRayDeriv( xNames = c( "land", "qLabor", "qVarInput" ),
+  yNames = c( "vCrop", "vAnimal" ),
+  coef = coef( estCD ), data = germanFarms, form = "cd" )
+lapply( derivCD, function(x) round( summary(x), 
+  3 - round( log( max( abs( x ) ), 10 ) ) ) )
+
+
+### Translog ray-based input distance function
 estTL <- suppressWarnings( distRayEst( xNames = c( "land", "qLabor", "qVarInput" ),
   yNames = c( "vCrop", "vAnimal" ),
   data = germanFarms ) )
@@ -23,3 +38,17 @@ cbind( names( estTL ) )
 lapply( estTL$coefList, function(x) round( x, 3 ) )
 apply( estTL$mono, 2, table )
 lapply( estTL$ela, function(x) round( summary(x), 2 ) )
+
+# calculate elasticities
+elaTL <- distRayEla( xNames = c( "land", "qLabor", "qVarInput" ),
+  yNames = c( "vCrop", "vAnimal" ),
+  coef = coef( estTL ), data = germanFarms )
+all.equal( elaTL, estTL$ela )
+lapply( elaTL, function(x) round( summary(x), 2 ) )
+
+# calculate derivatives
+derivTL <- distRayDeriv( xNames = c( "land", "qLabor", "qVarInput" ),
+  yNames = c( "vCrop", "vAnimal" ),
+  coef = coef( estTL ), data = germanFarms )
+lapply( derivTL, function(x) round( summary(x), 
+  3 - round( log( max( abs( x ) ), 10 ) ) ) )

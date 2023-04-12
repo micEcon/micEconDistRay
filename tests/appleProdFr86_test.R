06682f1d0eabd45ab187945b27587815d54e1d50
arne-henningsen <arne.henningsen@gmail.com>
2023-04-12 10:44:32 +0200

    Create appleProdFr86_test.R
    added test script that uses the data set "appleProdFr86"

---
+++ tests/appleProdFr86_test.R
@@ -0,0 +1,28 @@
+library( "micEconDistRay" )
+
+# load and prepare data set 
+data( appleProdFr86, package = "micEcon" )
+appleProdFr86$qCap <- appleProdFr86$vCap / appleProdFr86$pCap
+appleProdFr86$qLab <- appleProdFr86$vLab / appleProdFr86$pLab
+appleProdFr86$qMat <- appleProdFr86$vMat / appleProdFr86$pMat
+
+# Cobb-Douglas ray-based input distance function
+estCD <- distRayEst( xNames = c( "qCap", "qLab", "qMat" ),
+  yNames = c( "qApples", "qOtherOut" ),
+  data = appleProdFr86, form = "cd" )
+cbind( round( coef( estCD ), 2 ) )
+cbind( names( estCD ) )
+lapply( estCD$coefList, function(x) round( x, 3 ) )
+apply( estCD$mono, 2, table )
+lapply( estCD$ela, function(x) round( summary(x), 2 ) )
+
+# Translog ray-based input distance function
+estTL <- distRayEst( xNames = c( "qCap", "qLab", "qMat" ),
+  yNames = c( "qApples", "qOtherOut" ),
+  data = appleProdFr86 )
+cbind( round( coef( estTL ), 2 ) )
+cbind( names( estTL ) )
+lapply( estTL$coefList, function(x) round( x, 3 ) )
+apply( estTL$mono, 2, table )
+lapply( estTL$ela, function(x) round( summary(x), 2 ) )
+

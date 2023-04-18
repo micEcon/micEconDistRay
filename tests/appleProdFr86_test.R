e016be9cadd56cc73a6c67b5f5eece576ab2b2a7
arne-henningsen <arne.henningsen@gmail.com>
2023-04-18 16:20:09 +0200

    added tests for distRayMonoRestr()

--- tests/appleProdFr86_test.R
+++ tests/appleProdFr86_test.R
@@ -1,4 +1,5 @@
 library( "micEconDistRay" )
+library( "quadprog" )
 
 # load and prepare data set 
 data( appleProdFr86, package = "micEcon" )
@@ -36,6 +37,28 @@ derivCD <- distRayDeriv( xNames = c( "qCap", "qLab", "qMat" ),
 lapply( derivCD, function(x) round( summary(x), 
   3 - round( log( max( abs( x ) ), 10 ) ) ) )
 
+# vector of unrestricted coefficients and their covariance matrix
+nCoefCD <- length( coef( estCD ) ) - 2
+uCoefCD <- coef( estCD )[ 1:nCoefCD ]
+uCovInvCD <- solve( vcov( estCD )[ 1:nCoefCD, 1:nCoefCD ] )
+
+# obtain the matrix and vector to impose monotonicity
+restrCD <- distRayMonoRestr( xNames = c( "qCap", "qLab", "qMat" ),
+  yNames = c( "qApples", "qOtherOut" ), data = appleProdFr86,
+  form = "cd" )
+
+# obtain the restricted coefficients
+minDistCD <- solve.QP( Dmat = uCovInvCD, dvec = rep( 0, nCoefCD ),
+  Amat = t( restrCD$RMat ), bvec = - restrCD$RMat %*% uCoefCD + restrCD$rVec )
+rCoefCD <- minDistCD$solution + uCoefCD
+round( rCoefCD, 3 )
+
+# calculate elasticities based on restricted coefficients
+rElaCD <- distRayEla( xNames = c( "qCap", "qLab", "qMat" ),
+  yNames = c( "qApples", "qOtherOut" ),
+  coef = rCoefCD, data = appleProdFr86, form = "cd" )
+lapply( rElaCD, function(x) round( summary(x), 2 ) )
+
 
 ### Translog ray-based input distance function
 estTL <- distRayEst( xNames = c( "qCap", "qLab", "qMat" ),
@@ -66,4 +89,25 @@ derivTL <- distRayDeriv( xNames = c( "qCap", "qLab", "qMat" ),
   coef = coef( estTL ), data = appleProdFr86 )
 lapply( derivTL, function(x) round( summary(x), 
   3 - round( log( max( abs( x ) ), 10 ) ) ) )
+
+# vector of unrestricted coefficients and their covariance matrix
+nCoefTL <- length( coef( estTL ) ) - 2
+uCoefTL <- coef( estTL )[ 1:nCoefTL ]
+uCovInvTL <- solve( vcov( estTL )[ 1:nCoefTL, 1:nCoefTL ] )
+
+# obtain the matrix and vector to impose monotonicity
+restrTL <- distRayMonoRestr( xNames = c( "qCap", "qLab", "qMat" ),
+  yNames = c( "qApples", "qOtherOut" ), data = appleProdFr86 )
+
+# obtain the restricted coefficientslibrary( "quadprog" )
+minDistTL <- solve.QP( Dmat = uCovInvTL, dvec = rep( 0, nCoefTL ),
+  Amat = t( restrTL$RMat ), bvec = - restrTL$RMat %*% uCoefTL + restrTL$rVec )
+rCoefTL <- minDistTL$solution + uCoefTL
+round( rCoefTL, 3 )
+
+# calculate elasticities based on restricted coefficients
+rElaTL <- distRayEla( xNames = c( "qCap", "qLab", "qMat" ),
+  yNames = c( "qApples", "qOtherOut" ),
+  coef = rCoefTL, data = appleProdFr86 )
+lapply( rElaTL, function(x) round( summary(x), 2 ) )
 

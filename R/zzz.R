6359c7e5a8f9214fb51329bcd5251c815e29e233
arne-henningsen <arne.henningsen@gmail.com>
2023-05-11 11:14:08 +0200

    the start-up message of this package now refers to the working paper instead of to the forthcoming JPA paper

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,12 @@
 .onAttach <- function( libname, pkgname ) {
    packageStartupMessage(
-      paste0( "\nPlease cite the 'micEconDistRay' package as:\n",
-         "Price and Henningsen (forthcoming): ", 
+      paste0( "\nPlease cite the 'micEconDistRay' package in publications as:\n",
+         "Price, J.J. and Henningsen, A. (2022): ",
          "A Ray-Based Input Distance Function to Model Zero-Valued Output Quantities: ", 
          "Derivation and an Empirical Application. ",
-         "Journal of Productivity Analysis.\n\n",
+         "IFRO Working Paper 2022/03. ",
+         "Department of Food and Resource Economics, University of Copenhagen. ", 
+         "url: https://ideas.repec.org/p/foi/wpaper/2022_03.html.\n\n",
          "If you have questions, suggestions, or comments ",
          "regarding the 'micEconDistRay' package, ",
          "please use 'Issues' at the package's GitHub site:\n",

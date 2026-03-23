f075fff02f47e7bf4ba00332326a0f55ebca95ad
arne-henningsen <arne.henningsen@gmail.com>
2026-03-23 12:24:14 +0100

    updated the citation information
    (working paper -> article in the JPA)

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,12 @@
 .onAttach <- function( libname, pkgname ) {
    packageStartupMessage(
       paste0( "\nPlease cite the 'micEconDistRay' package in publications as:\n",
-         "Price, J.J. and Henningsen, A. (2022): ",
+         "Price, J.J. and Henningsen, A. (2023): ",
          "A Ray-Based Input Distance Function to Model Zero-Valued Output Quantities: ", 
          "Derivation and an Empirical Application. ",
-         "IFRO Working Paper 2022/03. ",
-         "Department of Food and Resource Economics, University of Copenhagen. ", 
-         "url: https://ideas.repec.org/p/foi/wpaper/2022_03.html.\n\n",
+         "Journal of Productivity Analysis 60, p. 179-188. ",
+         "DOI: 10.1007/s11123-023-00684-1. ",
+         "URL: https://doi.org/10.1007/s11123-023-00684-1.\n\n",
          "If you have questions, suggestions, or comments ",
          "regarding the 'micEconDistRay' package, ",
          "please use 'Issues' at the package's GitHub site:\n",
